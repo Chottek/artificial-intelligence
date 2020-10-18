3e59c9ac26f5dc871e4a7e334409985cec3dbea0
Chottek <49697145+Chottek@users.noreply.github.com>
2020-10-18 12:56:45 +0200

    Named Window in OpenCV

---
+++ namedWindows.cpp
@@ -0,0 +1,12 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui/highgui_c.h>
+#include <iostream>
+
+int main(int argc, char** argv){
+
+cv::namedWindow("Okienko", 1);
+cv::waitKey(0);
+return 0;
+
+
+}
