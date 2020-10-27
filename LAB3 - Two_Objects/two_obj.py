2b61506a470e7a496980d409c2f43704e9b16771
Chottek <49697145+Chottek@users.noreply.github.com>
2020-10-27 21:17:15 +0100

    Added two objects seeking and matching levels with red line

--- LAB3 - Two_Objects/two_obj.py
+++ LAB3 - Two_Objects/two_obj.py
@@ -6,6 +6,12 @@ greenUpper = (90, 255, 255)
 video = cv2.VideoCapture(0)
 
 while True:
+
+    firstBiggest = 0
+    secondBiggest = 0
+    firstContour = None
+    secondContour = None
+
     ret, frame = video.read()
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -18,10 +24,26 @@ while True:
 
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 255, 0), 2)
-            cv2.putText(imageFrame, "Match", (x, (y - 5)), cv2.FONT_ITALIC, 0.5, (0, 255, 0))
+
+        if area > 800:
+            if area > firstBiggest:
+                firstBiggest = area
+                firstContour = contour
+
+            elif firstBiggest > area > secondBiggest:
+                secondBiggest = area
+                secondContour = contour
+
+    x, y, w, h = cv2.boundingRect(firstContour)
+    imageFrame = cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 255, 0), 2)
+    cv2.putText(imageFrame, "Match", (x, (y - 5)), cv2.FONT_ITALIC, 0.5, (0, 255, 0))
+
+    x1, y1, w1, h1 = cv2.boundingRect(secondContour)
+    imageFrame1 = cv2.rectangle(frame, (x1, y1), ((x1 + w1), (y1 + h1)), (0, 255, 0), 2)
+    cv2.putText(imageFrame1, "Match", (x1, (y1 - 5)), cv2.FONT_ITALIC, 0.5, (0, 255, 0))
+
+    if y == y1 or y + h == y1 + h1:
+        cv2.line(imageFrame, (x + int(w / 2), y + int(h / 2)), (x1 + int(w1 / 2), y1 + int(h1 / 2)), (0, 0, 255), thickness=3, lineType=5)
 
     cv2.imshow("GreenDetector", frame)
 
