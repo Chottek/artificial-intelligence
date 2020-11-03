bf5007f5d5fb61ce0685b017d43fe06c7fd5c547
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-03 13:40:50 +0100

    Added file with ability to scan contours (i guess)

---
+++ LAB4 - Document Scanner/doc_scan.py
@@ -0,0 +1,40 @@
+import cv2
+
+url = "http://192.168.0.73:8080/video"
+
+video = cv2.VideoCapture(url)
+
+while True:
+
+    ret, frame = video.read()
+
+    frame = cv2.resize(frame, (680, 400))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+
+    contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    scr = None
+
+    for c in contours:
+        area = cv2.contourArea(c)
+
+        if area > 500:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+            if len(approx) == 4:
+                scr = approx
+                break
+
+    cv2.drawContours(frame, [scr], -1, (0, 0, 255), 2)
+    cv2.imshow("Contour", edged)
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+video.release()
+cv2.destroyAllWindows()
