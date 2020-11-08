ecf48e2302c71e21900e141dff84b4386959320a
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-08 13:12:22 +0100

    added some things here

--- LAB4 - Document Scanner/doc_scan.py
+++ LAB4 - Document Scanner/doc_scan.py
@@ -10,8 +10,51 @@
 
 # protip: cv2.rotate(image,image,cv2.rotate_90_clockwise);
 
+from datetime import datetime
+
 import cv2
 import numpy as np
+from imutils.perspective import order_points
+
+
+def transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+
 
 url = "http://192.168.0.73:8080/video"
 
@@ -32,10 +75,10 @@ while True:
 
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
+    scr = None
+
     for c in contours:
         area = cv2.contourArea(c)
-
-        scr = None
 
         if area > 800:
             peri = cv2.arcLength(c, True)
@@ -50,7 +93,6 @@ while True:
 
     if scr is not None:
         cv2.drawContours(frame, [scr], -1, (0, 0, 255), 5)
-        cv2.putText(frame, "Match", (int((x + w) / 2), int((y + h) / 2)), cv2.FONT_ITALIC, 1, (0, 0, 255))
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -58,9 +100,26 @@ while True:
     pink = cv2.erode(pink, None, iterations=2)
     pink = cv2.dilate(pink, None, iterations=2)
 
+    contours, hierarchy = cv2.findContours(pink, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    a = None
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if area > 300:
+            p = cv2.arcLength(contour, True)
+            a = cv2.approxPolyDP(contour, 0.02 * p, True)
+
+    if scr is not None and a is not None:
+        cv2.drawContours(frame, [a], -1, (0, 0, 255), 5)
+        img_name = "scrshot_{}.png".format(datetime.today().strftime('%Y%m%d%H%M%S'))
+        warped = transform(frame, #TODO: DO STH HERE! )
+        cv2.imwrite(img_name, warped)
+        # im = cv2.imread("scrshot_{}.png".format(counter))
+
     cv2.imshow("ContourSeeker", edged)
     cv2.imshow("RealView", frame)
-    cv2.imshow("NOR", pink)
 
     key = cv2.waitKey(1)
     if key == 27:
