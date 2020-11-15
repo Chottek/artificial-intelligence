2355c36d5dc2fb63651f3de9cac3ccda514faa78
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-15 16:01:07 +0100

    Added face cascade, made face seeking script fully working

---
+++ LAB5 - Object Finder/face_seeker.py
@@ -0,0 +1,29 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('cascade_face.xml')
+eye_cascade = cv2.CascadeClassifier('cascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faceSeeker = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faceSeeker:
+        img = cv2.circle(frame, (x + int(w / 2), y + int(h / 2)), int(w / 2), (0, 255, 255), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (x1, y1, w1, h1) in eyes:
+            cv2.rectangle(roi_color, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+
+    cv2.imshow("FaceSeeker", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
