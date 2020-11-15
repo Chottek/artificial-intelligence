7df304574a85ff059aed2573557354fe38d2724c
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-15 16:42:30 +0100

    added faces counter

--- LAB5 - Object Finder/face_seeker.py
+++ LAB5 - Object Finder/face_seeker.py
@@ -8,10 +8,13 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
+    numFaces = 0
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faceSeeker = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faceSeeker:
+        numFaces += 1
         img = cv2.circle(frame, (x + int(w / 2), y + int(h / 2)), int(w / 2), (0, 255, 255), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
@@ -19,6 +22,8 @@ while True:
 
         for (x1, y1, w1, h1) in eyes:
             cv2.rectangle(roi_color, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 2)
+
+    cv2.putText(frame, "Faces: {}".format(numFaces), (10, 15), cv2.FONT_ITALIC, 0.7, (0, 0, 0))
 
     cv2.imshow("FaceSeeker", frame)
 
