17b0d76deb41bd9085a8bad25a2609232cf4fd10
Chottek <49697145+Chottek@users.noreply.github.com>
2020-11-22 13:39:28 +0100

    Changed some code in car_seeker

--- LAB5 - Object Finder/car_seeker.py
+++ LAB5 - Object Finder/car_seeker.py
@@ -1,29 +1,54 @@
 import cv2
 
-
 cascade_src = 'cascade_car.xml'
+video_src = 'cars/cam_sur.mp4'
+
 car_cascade = cv2.CascadeClassifier(cascade_src)
 
-cap = cv2.VideoCapture("cars/Camera_surveillance.mp4")
+vid = cv2.VideoCapture(video_src)
+
+minCW = 20
+minCH = 20
+approx = 3
+line_height = 250
+matches = []
+
+
+def carCenter(x, y, w, h):
+    return x + int(w / 2), y + int(h / 2)
+
+
+carCount = 0
 
 while True:
-    ret, img = cap.read()
+    ret, frame = vid.read()
 
-    carsNum = 0
+    frame = cv2.resize(frame, (600, 400))
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.line(frame, (0, line_height), (3000, line_height), (0, 0, 255), 2)
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(gray, 1.1, 3)
 
     for (x, y, w, h) in cars:
-        carsNum += 1
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        if not [(w >= minCW) and (h >= minCH)]:
+            continue
 
-    cv2.putText(img, "Cars: {}".format(carsNum), (10, 15), cv2.FONT_ITALIC, 0.7, (0, 0, 0))
-    cv2.imshow('CarSeeker', img)
+        cv2.rectangle(frame, (x - approx, y - approx), (x + w + approx, y + h + approx), (0, 0, 255), 2)
+        matches.append(carCenter(x, y, w, h))
+
+    for (x, y) in matches:
+        if (line_height + approx) > y > (line_height - approx):
+            carCount += 1
+            cv2.line(frame, (0, line_height), (3000, line_height), (255, 255, 0), 2)
+            matches.remove((x, y))
+            continue
+
+    cv2.putText(frame, "Cars: {}".format(carCount), (10, 20), cv2.FONT_ITALIC, 0.6, (0, 0, 0), 1)
+    cv2.imshow('BigBrotherWatches', frame)
 
     if cv2.waitKey(1) == 27:
         break
 
+vid.release()
 cv2.destroyAllWindows()
-
