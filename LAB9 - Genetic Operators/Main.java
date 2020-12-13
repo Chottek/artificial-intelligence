ccf0c8b6526ed3eebb89065084be7a247ac263ad
Chottek <49697145+Chottek@users.noreply.github.com>
2020-12-13 16:13:55 +0100

    Initial Commit

---
+++ LAB9 - Genetic Operators/Main.java
@@ -0,0 +1,16 @@
+/**
+Napisz operatory genetyczne.
+Masz już przygotowane elementy zadania optymalizacyjnego do rozwiązania przez GA.Teraz czas na operatory genetyczne.Przygotuj i przetestuj:
+Krzyżowanie jednopunktowe
+Mutacja(wybierz)
+Selekcja turniejowa albo ruletkowa
+Tak więc po tych zajęciach powinno być prawie wszystko – bez samej głównej pętli algorytmu.To zrobimy na następnym naszym spotkaniu
+*/
+
+public class Main {
+    
+    public static void main(String... args) {
+
+    }
+
+}
