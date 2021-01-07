b89b849b8b628a85a4ebec01239fd1279c6b4660
Chottek <49697145+Chottek@users.noreply.github.com>
2021-01-07 11:20:25 +0100

    Added Controller and Neuron classes

---
+++ LAB11 - Neural Link/Controller.java
@@ -0,0 +1,12 @@
+public class Controller {
+
+    private static final double THRESHOLD = 0.0D;
+    private static final double LEARNING_RATE = 0.05;
+
+    public void run(){
+
+
+    }
+
+
+}
--- LAB11 - Neural Link/Launcher.java
+++ LAB11 - Neural Link/Launcher.java
@@ -1,7 +1,26 @@
+/**
+ * Przygotuj implementację sztucznego neuronu. Miej na uwadze fakt, że będzie trzeba je połączyć ze sobą na
+ * przyszłych zajęciach.
+ *
+ * Proszę o własną implementację, a nie kopiuj-wklej z przykładów.
+ * Jeśli pojawi się wiele takich samych rozwiązań to nie uznam ich.
+ *
+ * Inspiracja i pomysły
+ *
+ * Można zrobić to na macierzy i obsługiwać macierz.
+ * Na przykład wagi i połączenia między neuronami byłyby zrobione tak,
+ * jak w reprezentacji macierzy sąsiedztwa, gdzie wagi krawędzi to by były wagi wejść dla neuronów
+ * (https://pl.wikipedia.org/wiki/Reprezentacja_grafu).
+ * W takim wypadku będzie tylko jedna funkcja aktywacji.
+ * Dodatkowo trzeba będzie jakoś wskazać które neurony są wejściowe (będą przyjmowały wartość),
+ * a które są wyjściowe. Można zrobic odpowiednie listy.
+ */
+
+
 public class Launcher {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
